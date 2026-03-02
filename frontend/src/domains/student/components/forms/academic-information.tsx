2ce02187b3873cd0f4faba89fb8591e897324157
ashusalvi <ashusalvi281996@gmail.com>
2026-03-03 03:18:44 +0530

    Task completed by Ashitosh: Student Crud operation and notice board problem solve

--- frontend/src/domains/student/components/forms/academic-information.tsx
+++ frontend/src/domains/student/components/forms/academic-information.tsx
@@ -32,96 +32,94 @@ export const AcademicInformation = () => {
 
   return (
     <>
-  <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
-    <School sx={{ mr: 1 }} />
-    <Typography variant="body1">Academic Information</Typography>
-  </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
+        <School sx={{ mr: 1 }} />
+        <Typography variant='body1'>Academic Information</Typography>
+      </Box>
 
-  <Grid2 container spacing={2} sx={{ my: 2 }}>
-
-    {/* Class */}
-    <Grid2 size={{ xs: 12, md: 4 }}>
-      <FormControl fullWidth size="small" error={Boolean(errors.class)}>
-        <InputLabel id="class">Class</InputLabel>
-        <Controller
-          name="class"
-          control={control}
-          render={({ field }) => (
-            <Select {...field} label="Class">
-              {classes.map(({ name }) => (
-                <MenuItem key={name} value={name}>
-                  {name}
-                </MenuItem>
-              ))}
-            </Select>
-          )}
-        />
-        <FormHelperText>{errors.class?.message}</FormHelperText>
-      </FormControl>
-    </Grid2>
-
-    {/* Section */}
-    <Grid2 size={{ xs: 12, md: 4 }}>
-      <FormControl fullWidth size="small" error={Boolean(errors.section)}>
-        <InputLabel id="section">Section</InputLabel>
-        <Controller
-          name="section"
-          control={control}
-          render={({ field }) => (
-            <Select {...field} label="Section">
-              {isLoading ? (
-                <MenuItem disabled>Loading...</MenuItem>
-              ) : (
-                data?.sections?.map(({ name }) => (
-                  <MenuItem key={name} value={name}>
-                    {name}
-                  </MenuItem>
-                ))
+      <Grid2 container spacing={2} sx={{ my: 2 }}>
+        {/* Class */}
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <FormControl fullWidth size='small' error={Boolean(errors.class)}>
+            <InputLabel id='class'>Class</InputLabel>
+            <Controller
+              name='class'
+              control={control}
+              render={({ field }) => (
+                <Select {...field} label='Class'>
+                  {classes.map(({ name }) => (
+                    <MenuItem key={name} value={name}>
+                      {name}
+                    </MenuItem>
+                  ))}
+                </Select>
               )}
-            </Select>
-          )}
-        />
-        <FormHelperText>{errors.section?.message}</FormHelperText>
-      </FormControl>
-    </Grid2>
+            />
+            <FormHelperText>{errors.class?.message}</FormHelperText>
+          </FormControl>
+        </Grid2>
 
-    {/* Roll */}
-    <Grid2 size={{ xs: 12, md: 4 }}>
-      <TextField
-        {...register('roll')}
-        fullWidth
-        label="Roll"
-        error={Boolean(errors.roll)}
-        helperText={errors.roll?.message}
-        size="small"
-      />
-    </Grid2>
+        {/* Section */}
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <FormControl fullWidth size='small' error={Boolean(errors.section)}>
+            <InputLabel id='section'>Section</InputLabel>
+            <Controller
+              name='section'
+              control={control}
+              render={({ field }) => (
+                <Select {...field} label='Section'>
+                  {isLoading ? (
+                    <MenuItem disabled>Loading...</MenuItem>
+                  ) : (
+                    data?.sections?.map(({ name }) => (
+                      <MenuItem key={name} value={name}>
+                        {name}
+                      </MenuItem>
+                    ))
+                  )}
+                </Select>
+              )}
+            />
+            <FormHelperText>{errors.section?.message}</FormHelperText>
+          </FormControl>
+        </Grid2>
 
-    {/* Admission Date */}
-    <Grid2 size={{ xs: 12, md: 4 }}>
-      <Controller
-        name="admissionDate"
-        control={control}
-        render={({ field: { onChange, value }, fieldState: { error } }) => (
-          <DatePicker
-            label="Admission Date"
-            format={DATE_FORMAT}
-            value={typeof value === 'string' ? parseISO(value) : value}
-            onChange={(newDate) => onChange(newDate)}
-            slotProps={{
-              textField: {
-                fullWidth: true,
-                size: 'small',
-                error: Boolean(error),
-                helperText: error?.message
-              }
-            }}
+        {/* Roll */}
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <TextField
+            {...register('roll')}
+            fullWidth
+            label='Roll'
+            error={Boolean(errors.roll)}
+            helperText={errors.roll?.message}
+            size='small'
           />
-        )}
-      />
-    </Grid2>
+        </Grid2>
 
-  </Grid2>
-</>
+        {/* Admission Date */}
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <Controller
+            name='admissionDate'
+            control={control}
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+              <DatePicker
+                label='Admission Date'
+                format={DATE_FORMAT}
+                value={typeof value === 'string' ? parseISO(value) : value}
+                onChange={(newDate) => onChange(newDate)}
+                slotProps={{
+                  textField: {
+                    fullWidth: true,
+                    size: 'small',
+                    error: Boolean(error),
+                    helperText: error?.message
+                  }
+                }}
+              />
+            )}
+          />
+        </Grid2>
+      </Grid2>
+    </>
   );
 };
--- frontend/src/domains/student/components/forms/basic-information.tsx
+++ frontend/src/domains/student/components/forms/basic-information.tsx
@@ -29,109 +29,107 @@ export const BasicInformation = () => {
 
   return (
     <>
-  <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
-    <AccountCircle sx={{ mr: 1 }} />
-    <Typography variant="body1">Basic Information</Typography>
-  </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
+        <AccountCircle sx={{ mr: 1 }} />
+        <Typography variant='body1'>Basic Information</Typography>
+      </Box>
 
-  <Grid2 container spacing={2} sx={{ my: 2 }}>
-    
-    {/* Full Name */}
-    <Grid2 size={{ xs: 12, md: 4 }}>
-      <TextField
-        {...register('name')}
-        fullWidth
-        label="Full Name"
-        error={Boolean(errors.name)}
-        helperText={errors.name?.message}
-        size="small"
-      />
-    </Grid2>
+      <Grid2 container spacing={2} sx={{ my: 2 }}>
+        {/* Full Name */}
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <TextField
+            {...register('name')}
+            fullWidth
+            label='Full Name'
+            error={Boolean(errors.name)}
+            helperText={errors.name?.message}
+            size='small'
+          />
+        </Grid2>
 
-    {/* Phone */}
-    <Grid2 size={{ xs: 12, md: 4 }}>
-      <TextField
-        {...register('phone')}
-        fullWidth
-        label="Phone Number"
-        error={Boolean(errors.phone)}
-        helperText={errors.phone?.message}
-        size="small"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <Call fontSize="small" />
-            </InputAdornment>
-          )
-        }}
-      />
-    </Grid2>
-
-    {/* Email */}
-    <Grid2 size={{ xs: 12, md: 4 }}>
-      <TextField
-        {...register('email')}
-        fullWidth
-        label="Email"
-        error={Boolean(errors.email)}
-        helperText={errors.email?.message}
-        size="small"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <Email fontSize="small" />
-            </InputAdornment>
-          )
-        }}
-      />
-    </Grid2>
-
-    {/* DOB */}
-    <Grid2 size={{ xs: 12, md: 4 }}>
-      <Controller
-        name="dob"
-        control={control}
-        render={({ field: { onChange, value }, fieldState: { error } }) => (
-          <DatePicker
-            label="Birth Date"
-            format={DATE_FORMAT}
-            value={typeof value === 'string' ? parseISO(value) : value}
-            onChange={(newDt) => onChange(newDt)}
-            slotProps={{
-              textField: {
-                fullWidth: true,
-                size: 'small',
-                error: Boolean(error),
-                helperText: error?.message
-              }
+        {/* Phone */}
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <TextField
+            {...register('phone')}
+            fullWidth
+            label='Phone Number'
+            error={Boolean(errors.phone)}
+            helperText={errors.phone?.message}
+            size='small'
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position='start'>
+                  <Call fontSize='small' />
+                </InputAdornment>
+              )
             }}
           />
-        )}
-      />
-    </Grid2>
+        </Grid2>
 
-    {/* Gender */}
-    <Grid2 size={{ xs: 12, md: 4 }}>
-      <FormControl fullWidth size="small" error={Boolean(errors.gender)}>
-        <InputLabel id="gender">Gender</InputLabel>
-        <Controller
-          name="gender"
-          control={control}
-          render={({ field }) => (
-            <Select {...field} label="Gender">
-              {genders.map((gender) => (
-                <MenuItem key={gender.id} value={gender.id}>
-                  {gender.name}
-                </MenuItem>
-              ))}
-            </Select>
-          )}
-        />
-        <FormHelperText>{errors.gender?.message}</FormHelperText>
-      </FormControl>
-    </Grid2>
+        {/* Email */}
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <TextField
+            {...register('email')}
+            fullWidth
+            label='Email'
+            error={Boolean(errors.email)}
+            helperText={errors.email?.message}
+            size='small'
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position='start'>
+                  <Email fontSize='small' />
+                </InputAdornment>
+              )
+            }}
+          />
+        </Grid2>
 
-  </Grid2>
-</>
+        {/* DOB */}
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <Controller
+            name='dob'
+            control={control}
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+              <DatePicker
+                label='Birth Date'
+                format={DATE_FORMAT}
+                value={typeof value === 'string' ? parseISO(value) : value}
+                onChange={(newDt) => onChange(newDt)}
+                slotProps={{
+                  textField: {
+                    fullWidth: true,
+                    size: 'small',
+                    error: Boolean(error),
+                    helperText: error?.message
+                  }
+                }}
+              />
+            )}
+          />
+        </Grid2>
+
+        {/* Gender */}
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <FormControl fullWidth size='small' error={Boolean(errors.gender)}>
+            <InputLabel id='gender'>Gender</InputLabel>
+            <Controller
+              name='gender'
+              control={control}
+              render={({ field }) => (
+                <Select {...field} label='Gender'>
+                  {genders.map((gender) => (
+                    <MenuItem key={gender.id} value={gender.id}>
+                      {gender.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              )}
+            />
+            <FormHelperText>{errors.gender?.message}</FormHelperText>
+          </FormControl>
+        </Grid2>
+      </Grid2>
+    </>
   );
 };
